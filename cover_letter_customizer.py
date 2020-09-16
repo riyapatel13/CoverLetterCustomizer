1c4c743e6984242393a8eed7b45cba45799c7000
riyapatel13 <riaupatel13@yahoo.com>
2020-09-16 13:15:11 -0400

    Included code for merging cover letter with resume

--- cover_letter_customizer.py
+++ cover_letter_customizer.py
@@ -1,8 +1,5 @@
 import argparse
-from PyPDF2 import PdfFileReader
-from reportlab.pdfgen.canvas import Canvas
-from reportlab.lib.units import inch, cm
-from pathlib import Path
+import PyPDF2
 from datetime import date
 from fpdf import FPDF
 import os
@@ -53,6 +50,7 @@ def setup_dir(dir_name):
             os.mkdir(dir_name)
     return dir_name
 
+# customizes pdf and returns path to cover letter
 def customize_pdf(template, date, company, name, position, cover_letter_dir):
     # customize string
     template = template.replace("[Date]", date)
@@ -64,12 +62,29 @@ def customize_pdf(template, date, company, name, position, cover_letter_dir):
     pdf.add_page()
     pdf.set_font('Times', '', 12)
     pdf.multi_cell(0, 0.25, template)
-    if cover_letter_dir != None:
-        pdf.output(f'{cover_letter_dir}/{company}_cover_letter.pdf', 'F').encode("latin-1")
-    else: pdf.output(f'{company}_cover_letter.pdf', 'F').encode("latin-1")
 
-    return
-   
+    if cover_letter_dir != None:
+        cover_letter_name = f'{cover_letter_dir}/{company}_cover_letter.pdf'
+    else: cover_letter_name = f'{company}_cover_letter.pdf'
+
+    pdf.output(cover_letter_name, 'F').encode("latin-1")
+
+    return cover_letter_name
+
+def merge_pdf(letter_path, resume_path, cover_letter_dir):
+    merger = PyPDF2.PdfFileMerger()
+    merger.append(letter_path)
+    merger.append(resume_path)
+
+    # naming the file
+    if cover_letter_dir != None:
+        file_name = f'{cover_letter_dir}/{company}_cover_letter_resume_merged.pdf'
+    else: file_name = f'{company}_cover_letter_resume_merged.pdf'
+
+    merger.write(file_name)
+    merger.close()
+
+
 if __name__ == "__main__":
     
     argparser = argparse.ArgumentParser(description = "Cover Letter Customizer - Given a list of companies you want to apply to (line-separated txt file) and a cover letter template (txt), this file will create a folder with customized cover letters for each company.")
@@ -93,15 +108,31 @@ if __name__ == "__main__":
 
     cover_letter_dir = input("Please enter a folder name to save all the cover letters (Optional - hit Enter for no folder):\n")
     cover_letter_dir = setup_dir(cover_letter_dir)
+    merge = input("Would you like to merge your cover letter with your resume (Y/N)?\n")
+
+    if merge == 'Y': merge = True
+    else: merge = False
+
+    if merge:
+        resume_path = input("Type in the relative path of your resume:\n")
 
     if args.companies_file is None:
-        customize_pdf(tem, cur_date, company, name, position, cover_letter_dir)
+        cover_letter_path = customize_pdf(tem, cur_date, company, name, position, cover_letter_dir)
+        print(cover_letter_path)
+        print(resume_path)
+        if merge:
+            merge_pdf(cover_letter_path, resume_path, cover_letter_dir)
     else:
         companies = list_companies(args.companies_file)
         for company in companies:
-            customize_pdf(tem, cur_date, company, name, position, cover_letter_dir)
+            cover_letter_path = customize_pdf(tem, cur_date, company, name, position, cover_letter_dir)
+            if merge:
+                merge_pdf(cover_letter_path, resume_path, cover_letter_dir)
+    
     
     if cover_letter_dir == None:
         print(f"Your customized cover letter(s) are saved in the current folder")
     else: 
         print(f"Your customized cover letter(s) are saved in the folder {cover_letter_dir}")
+    if merge:
+        print("Your merged letter is stored in the same folder.")
